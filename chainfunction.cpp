007345aa0498ebb364306028c6a1f45e562a8a3c
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-05 17:26:48 +0700

    Add files via upload

---
+++ chainfunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class buku
+{
+    string judul;
+
+    public:
+
+    buku &setJudul(string judul)
+    {
+        this -> judul = judul; 
+        return *this; //chain function
+    }
+    string getJudul()
+    {
+        return this -> judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    //bukunya.setJudul('matematika');
+    //cout << bukunya.getJudul() << endl;
+    cout << bukunya.setJudul("matematika").getJudul(); //chain function calls
+    return 0;
+}
