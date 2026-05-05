a2784477a38fe9a2a65286670a0e4531c1fd5230
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-05 17:33:22 +0700

    Refactor mahasiswa class constructor definitions

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -35,22 +35,3 @@ mahasiswa :: mahasiswa(int iNim, string iNama)
     nama = iNama;
 }
 
-void mahasiswa::cetak()
-{
-    cout << endl << "Nim =" << nim << endl;
-    cout << "nama =" << nama << endl;
-}
-
-int main () 
-{
-    mahasiswa mhs1(102030); //hanya NIM
-    mahasiswa mhs2("Faid"); //hanya nama
-    mahasiswa mhs3(010203, "Yuratama"); //hanya NIM dan nama
-
-    //tampilkan nilai
-    cout << "Mahasiswa 1 :"; mhs1.cetak();
-    cout << "Mahasiswa 2 :"; mhs2.cetak();
-    cout << "Mahasiswa 3 :"; mhs3.cetak();
-
-    return 0;
-}
